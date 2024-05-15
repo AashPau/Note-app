10c0c4975bdacc11cdad20762570b21da8908657
Aash <31497659+AashPau@users.noreply.github.com>
2024-05-15 13:45:36 +0000

    initialize new component and add bootstrap

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Card = () => {
+  return (
+    <div className=" container d-flex justify-content-center align-items-center m-auto mt-5">
+      <textarea />
+      <textarea />
+    </div>
+  );
+};
